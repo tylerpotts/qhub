cd437305777505a9bbd2adda10b3e7529a744afa
viniciusdc <vinivdc2009@hotmail.com>
2022-03-14 17:23:09 -0300

    Change container registry name

--- qhub/template/stages/02-infrastructure/azure/modules/registry/main.tf
+++ qhub/template/stages/02-infrastructure/azure/modules/registry/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "container_registry" {
-  name                = var.name
+  name                = "QhubContainerRergistry"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Standard"
