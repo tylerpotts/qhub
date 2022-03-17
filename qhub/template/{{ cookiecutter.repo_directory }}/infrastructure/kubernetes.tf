23e0b236ac8d2a324d7b956038e15d4187acc119
viniciusdc <vinivdc2009@hotmail.com>
2022-03-17 10:29:47 -0300

    Fix LB call

--- qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kubernetes.tf
+++ qhub/template/{{ cookiecutter.repo_directory }}/infrastructure/kubernetes.tf
@@ -164,18 +164,16 @@ module "kubernetes-ingress" {
 
   node-group = local.node_groups.general
 
-{% if cookiecutter.load_balancer.ip_address is defined %}
+{% if cookiecutter.load_balancer.ip_address != "" %}
   load-balancer-ip = "{{ cookiecutter.load_balancer.ip_address }}"
 {% endif %}
 
-{%- if cookiecutter.load_balancer is defined %}
-{% if cookiecutter.load_balancer.annotations != {} %}
+{% if cookiecutter.load_balancer.annotations is defined and not {} %}
   load-balancer-annotations = {
 {%- for key, value in cookiecutter.load_balancer.annotations.items() %}
-    "{{ "%s" | format(key) }}" = "{{ value }}"
+    "{{ '%s' | format(key) }}" = "{{ value }}"
 {% endfor -%}
-  }
-{% endif -%}
+    }
 {% endif -%}
 
 {% if cookiecutter.certificate.type == "lets-encrypt" %}
